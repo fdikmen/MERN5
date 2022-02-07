cd92721c2adfc8e3f324cf06bd7f2aafb5cb8a6c
frtdkmn <frtdkmn@gmail.com>
2022-02-07 23:26:48 +0300

    Added lesson 5 codes added part

---
+++ Lesson05/phonebook/src/components/MyMathLib.js
@@ -0,0 +1,17 @@
+export const Topla = () => {
+  return <div></div>;
+};
+
+export const Carp = () => {
+  return <div></div>;
+};
+
+export const Cikar = () => {
+  return <div></div>;
+};
+
+export const Bol = () => {
+  return <div></div>;
+};
+
+export default Topla
---
+++ Lesson05/phonebook/src/components/UsingMath.js
@@ -0,0 +1,10 @@
+import YourMath,{Cikar,Bol,Car} from './MyMathLib'
+
+import React from 'react'
+import Other from 'express'
+
+
+import '../App.css'
+
+
+
