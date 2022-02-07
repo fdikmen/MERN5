d6e3038946e9f25a5604c077bb44ca574ebb3057
frtdkmn <frtdkmn@gmail.com>
2022-02-07 22:58:05 +0300

    Added lesson 5 codes part last

--- Lesson05/phonebook/src/components/Contact.js
+++ Lesson05/phonebook/src/components/Contact.js
@@ -4,10 +4,17 @@ import List from './List';
 import Form from './Form';
 
 class Contact extends Component {
+  state={
+    contacts:[{name:"Name 1",phone:"+123456789"},
+    {name:"Name 2",phone:"+123456780"},
+    {name:"Name 3",phone:"+123456781"},
+    {name:"Name 4",phone:"+123456782"},
+    {name:"Name 5",phone:"+123456783"}]
+  }
   render() {
     return <div>
         <h1>Contacts</h1>
-        <List/>
+        <List contacts={this.state.contacts}/>
         <Form/>
     </div>;
   }
--- Lesson05/phonebook/src/components/List.js
+++ Lesson05/phonebook/src/components/List.js
@@ -1,25 +1,33 @@
 //Created by "rcc" snippet
-import React, { Component } from 'react';
-import './List.css'
+import React, { Component } from "react";
+import "./List.css";
+import PropsTypes from 'prop-types'
 
 export default class List extends Component {
+    static propTypes = {
+        contacts:PropsTypes.array.isRequired
+    }
   render() {
-    return <div>
-        <input className='filter-input' name="filter" id="filter" 
-        placeholder='Filter by name or phone...'>            
-        </input>
-        <ul className='listField'>
-            <li>
-                <span className='name'>Name 1</span>
-                <span className='phone'>Phone 1</span>
-            </li>
-            <li>
-                <span className='name'>Name 2</span>
-                <span className='phone'>Phone 2</span></li>
-            <li>
-                <span className='name'>Name 3</span>
-                <span className='phone'>Phone 3</span></li>
+    console.log(this.props);
+    return (
+      <div>
+        <input
+          className="filter-input"
+          name="filter"
+          id="filter"
+          placeholder="Filter by name or phone..."
+        ></input>
+        <ul className="listField">
+          {this.props.contacts.map((item,index) => {
+            return (
+              <li key={index}>
+                <span className="name">{item.name}</span>
+                <span className="phone">{item.phone}</span>
+              </li>
+            );
+          })}
         </ul>
-    </div>;
+      </div>
+    );
   }
 }
