39cf9738c367d1f132b4aa0fa9c586d2faa51046
frtdkmn <frtdkmn@gmail.com>
2022-02-09 21:06:36 +0300

    Lesson 06 added codes part1.

--- Lesson05/phonebook/src/components/Form.js
+++ Lesson05/phonebook/src/components/Form.js
@@ -1,19 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Form extends Component {
+  state = { name: "", phone: "" };
+  /*constructor(props) {
+    super(props);
+    this.onChangeFunc = this.onChangeFunc.bind(this);
+  }
+  onChangeFunc(e) {
+    console.log("Writing....",e.target.value);
+    if(e.target.name==='name'){
+      this.setState({name:e.target.value})
+    }
+    if(e.target.name==='phone'){
+      this.setState({phone:e.target.value})
+    }
+    this.setState({[e.target.name]:e.target.value})
+  }*/
   render() {
-      const formInputStyle={marginTop: "10px",
-        height: "30px",
-        lineHeight: "25px",
-        width: "400px",
-        textIndent: "10px"}
-    return <div>
-        <form style={{width:"400px",margin:"0 auto",padding:"0"}}>
-            <input style={formInputStyle} name="name" id="name" placeholder='Enter a name...'></input>
-            <br/>
-            <input style={formInputStyle} name="phone" id="phone" placeholder='Enter a phone...'></input>
-        <button>Add</button>
+    const onChangeFunc=(e)=> {     this.setState({[e.target.name]:e.target.value})    }
+    const formInputStyle = {
+      marginTop: "10px",
+      height: "30px",
+      lineHeight: "25px",
+      width: "400px",
+      textIndent: "10px",
+    };
+    return (
+      <div>
+        <form style={{ width: "400px", margin: "0 auto", padding: "0" }}>
+          <input
+            style={formInputStyle}
+            value={this.state.name}
+            onChange={onChangeFunc}
+            name="name"
+            id="name"
+            placeholder="Enter a name..."
+          ></input>
+          <br />
+          <input
+            style={formInputStyle}
+            value={this.state.phone}
+            onChange={onChangeFunc}
+            name="phone"
+            id="phone"
+            placeholder="Enter a phone..."
+          ></input>
+          <button>Add</button>
         </form>
-    </div>;
+      </div>
+    );
   }
 }
