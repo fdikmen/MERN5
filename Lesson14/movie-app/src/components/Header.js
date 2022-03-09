62c6f1e03ce0691931f4e454cc356f8ab0540706
frtdkmn <frtdkmn@gmail.com>
2022-03-09 23:05:52 +0300

    Added lesson 14 codes.

---
+++ Lesson14/movie-app/src/components/Header.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { Container, Image, Menu } from "semantic-ui-react";
+import { fixedMenuStyle, menuStyle } from "../helpers/styleHelpers";
+
+export default class Header extends Component {
+  state = {
+    menuFixed: false,
+    overlayFixed: false,
+  };
+  render() {
+    const { menuFixed } = this.state;
+    return (
+      <Menu
+        borderless
+        fixed={menuFixed ? "top" : undefined}
+        style={menuFixed ? fixedMenuStyle : menuStyle}
+      >
+        <Container text>
+          <Menu.Item>
+            <Image size="mini" src="https://react.semantic-ui.com/logo.png" />
+          </Menu.Item>
+          <Menu.Item header>MovieApp</Menu.Item>
+          <Menu.Item as={Link} to="/">
+            HomePage
+          </Menu.Item>
+          <Menu.Item as={Link} to="movies">
+            Movies
+          </Menu.Item>
+        </Container>
+      </Menu>
+    );
+  }
+}
